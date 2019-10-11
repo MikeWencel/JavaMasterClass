f6375880af331da4544c0cdc82d728c698a28557
MikeWen <wencel.michael@gmail.com>
2019-10-11 09:24:05 +0200

    Remastering - int

---
+++ SectionOne/Variables/src/com/mike_wen/Main.java
@@ -0,0 +1,16 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myFirstNumber = 5 + 2 * 3;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("Total of number: " + myTotal);
+        int lastOne = 1000 - myTotal;
+        System.out.println("My last number: " + lastOne);
+    }
+
+}
