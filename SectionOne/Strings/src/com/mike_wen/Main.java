d5fe071cd021f17eba5021865815997f5ae4c1c0
MikeWen <wencel.michael@gmail.com>
2019-10-12 11:34:41 +0200

    String

---
+++ SectionOne/Strings/src/com/mike_wen/Main.java
@@ -0,0 +1,18 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String myString ="This is a string";
+
+        System.out.println("mystring is equal to " + myString);
+        myString = myString + ",and this is more.";
+        System.out.println("myString is equal to " + myString);
+        double number = 50.50;
+        myString = "50.50";
+
+        System.out.println(myString + number);
+    }
+}
