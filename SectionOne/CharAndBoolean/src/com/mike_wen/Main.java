50a744903aa5b90476f08c0bd023bad795372ceb
MikeWen <wencel.michael@gmail.com>
2019-10-12 11:25:11 +0200

    Char and Unicode

--- SectionOne/CharAndBoolean/src/com/mike_wen/Main.java
+++ SectionOne/CharAndBoolean/src/com/mike_wen/Main.java
@@ -7,5 +7,12 @@ public class Main {
         char myChar ='\u0040';
 
         System.out.println(myChar);
+
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+
     }
 }
