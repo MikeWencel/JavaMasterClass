e32d8dcebfa0cbe98758e9c86629e0aa7025cb28
MikeWen <wencel.michael@gmail.com>
2019-10-12 11:45:18 +0200

    Operators and abbreviating

---
+++ SectionOne/Operators/src/com/mike_wen/Main.java
@@ -0,0 +1,20 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        result = result - 2; // 1
+        System.out.println("3 - 2 = " + result);
+        result = result * 10;
+        System.out.println("1 * 10 = " + result);
+        result = result / 2;
+        System.out.println("10 / 2 = " + result);
+        result = result % 3;
+        System.out.println("5 % 3 = " + result);
+
+        result++;
+        System.out.println("2 + 2 = " + result);
+    }
+}
