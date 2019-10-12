72909de40ef8fc18bddd4ec285bb71031a480715
MikeWen <wencel.michael@gmail.com>
2019-10-13 01:11:32 +0200

    Methods - important clean coding. Nice shortcut

--- SectionTwo/ifElseAnd/src/com/mike_wen/Main.java
+++ SectionTwo/ifElseAnd/src/com/mike_wen/Main.java
@@ -3,27 +3,22 @@ package com.mike_wen;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        
 
-        if (gameOver){
-            int finalScore = score * bonus;
-            finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(true,800,5,100);
+        calculateScore(true,10000,8,200);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
 
-        if (gameOver){
-            int finalScore = score * bonus;
-            finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
-        }
     }
+
+    public static void calculateScore(boolean gameOver,int score,int levelCompleted,int bonus){
+
+            if (gameOver){
+                int finalScore = score * bonus;
+                finalScore += 1000;
+                System.out.println("Your final score was " + finalScore);
+            }
+        }
 
 
 }
