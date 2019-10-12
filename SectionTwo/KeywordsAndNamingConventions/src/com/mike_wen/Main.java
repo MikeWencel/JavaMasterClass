3a6e3287d4bf5f6e574a14f8e52cdd0439507ecf
MikeWen <wencel.michael@gmail.com>
2019-10-12 13:18:38 +0200

    Challenge naming convensions

---
+++ SectionTwo/KeywordsAndNamingConventions/src/com/mike_wen/Main.java
@@ -0,0 +1,11 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double drivenKilometers;
+        int customerVisitPerDay;
+        String lastKeyPush;
+    }
+}
