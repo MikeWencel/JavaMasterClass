3e0ad5403255f7cde70fcb6b2951681ae4990414
MikeWen <wencel.michael@gmail.com>
2019-10-13 20:14:15 +0200

    Equal simply one for relax :-)

---
+++ SectionTwo/EqualSumChecker/src/com/mike_wen/Main.java
@@ -0,0 +1,15 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static boolean hasEqualSum(int first,int second,int third){
+        int total = first + second;
+        if (total == third){
+            return true;
+        }return false;
+    }
+}
