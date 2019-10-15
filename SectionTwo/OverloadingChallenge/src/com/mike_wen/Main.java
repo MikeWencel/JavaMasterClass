cefc889bd135607fb6df1381769c286d2e8ef703
MikeWen <wencel.michael@gmail.com>
2019-10-15 21:08:32 +0200

    Method overloading - Everything done

--- SectionTwo/OverloadingChallenge/src/com/mike_wen/Main.java
+++ SectionTwo/OverloadingChallenge/src/com/mike_wen/Main.java
@@ -3,21 +3,26 @@ package com.mike_wen;
 public class Main {
 
     public static void main(String[] args) {
-    calcFeetAndInchesToCentimeteres(3,4);
+    calcFeetAndInchesToCentimeteres(157);
     }
 
     public static double calcFeetAndInchesToCentimeteres(double feet,double inches){
         if ((feet >= 0) && inches >= 0 && inches <= 12 ){
             double centimeters = (feet * 12) * 2.54;
-            double secNewInches = inches * 2.54;
-            System.out.println(centimeters + secNewInches + " cm");
-        }return -1;
+            centimeters += inches * 2.54;
+            System.out.println(feet + " feet " + inches + " inches " + centimeters + " cm");
+            return centimeters;
+        }
+        System.out.println("Invalid parameter");
+        return -1;
     }
 
     public static double calcFeetAndInchesToCentimeteres (double inches){
         if (inches >= 0){
-        double feets = inches/12;
-            System.out.println("Feets = " + feets);
+        double feet = (int) inches/12;
+        double remainderInches = (int) inches % 12;
+        System.out.println(inches + " inches = " + feet + " ft " + remainderInches + " inches");
+        return calcFeetAndInchesToCentimeteres(feet,remainderInches);
         }return -1;
     }
 }
