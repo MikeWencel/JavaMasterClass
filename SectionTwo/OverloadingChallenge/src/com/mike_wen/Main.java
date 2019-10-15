a07d99fa4270e33a1e44f3686599434246411b2d
MikeWen <wencel.michael@gmail.com>
2019-10-15 20:52:43 +0200

    Method overloading - first method done. Works great !

--- SectionTwo/OverloadingChallenge/src/com/mike_wen/Main.java
+++ SectionTwo/OverloadingChallenge/src/com/mike_wen/Main.java
@@ -3,20 +3,21 @@ package com.mike_wen;
 public class Main {
 
     public static void main(String[] args) {
-    calcFeetAndInchesToCentimeteres(3,0);
+    calcFeetAndInchesToCentimeteres(3,4);
     }
 
     public static double calcFeetAndInchesToCentimeteres(double feet,double inches){
         if ((feet >= 0) && inches >= 0 && inches <= 12 ){
-            double newInches = feet * 12;
-            newInches = newInches * 2.54;
-            System.out.println(newInches + " cm");
+            double centimeters = (feet * 12) * 2.54;
+            double secNewInches = inches * 2.54;
+            System.out.println(centimeters + secNewInches + " cm");
         }return -1;
     }
 
     public static double calcFeetAndInchesToCentimeteres (double inches){
         if (inches >= 0){
-            
+        double feets = inches/12;
+            System.out.println("Feets = " + feets);
         }return -1;
     }
 }
