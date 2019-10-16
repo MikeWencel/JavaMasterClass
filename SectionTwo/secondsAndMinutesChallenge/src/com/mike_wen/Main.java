c77aa6c86f5fcfec882adc5a8b451fe763fb73ed
MikeWen <wencel.michael@gmail.com>
2019-10-16 21:01:25 +0200

    Everything is working very well ! :)

--- SectionTwo/secondsAndMinutesChallenge/src/com/mike_wen/Main.java
+++ SectionTwo/secondsAndMinutesChallenge/src/com/mike_wen/Main.java
@@ -3,7 +3,7 @@ package com.mike_wen;
 public class Main {
 
     public static void main(String[] args) {
-	getDurationString(150,50);
+	getDurationString(80000);
     }
 
     public static int getDurationString(int minutes,int seconds){
@@ -11,6 +11,15 @@ public class Main {
             int hours = minutes / 60;
             int remainderMinutes = minutes % 60;
             System.out.println(hours + "h " + remainderMinutes +"m " + seconds +"s ");
+        }return -1;
+    }
+
+    public static int getDurationString(int seconds){
+        if (seconds >= 0){
+            int newMinutes = seconds / 60;
+            int remainderSeconds = seconds % 60;
+            System.out.println(seconds+"s " + " is: " + newMinutes + "m " + remainderSeconds + "s");
+            return getDurationString(newMinutes,remainderSeconds);
         }return -1;
     }
 }
