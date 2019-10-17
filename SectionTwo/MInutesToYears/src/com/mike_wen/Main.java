69c79ef3043dd2ddbb3ca9bbffeb8add75d6d61f
MikeWen <wencel.michael@gmail.com>
2019-10-17 21:04:28 +0200

    Done

--- SectionTwo/MInutesToYears/src/com/mike_wen/Main.java
+++ SectionTwo/MInutesToYears/src/com/mike_wen/Main.java
@@ -3,16 +3,18 @@ package com.mike_wen;
 public class Main {
 
     public static void main(String[] args) {
-	printYearsAndDays(525600);
+	printYearsAndDays(561600);
     }
 
     public static void printYearsAndDays(long minutes){
-        if (minutes > 0){
-        int days = (int) minutes / 3600;
+        if (minutes >= 0){
+        int hour = (int) minutes / 60;
+        int days = hour / 24;
+        int newDays = days % 365;
         int years = days / 365;
-            System.out.println(minutes + " min = " + years + " years " + days + " days");
+            System.out.println(minutes + " min = " + years + " y and " + newDays + " d");
         }else {
-            System.out.println("Invalid value");
+            System.out.println("Invalid Value");
         }
 
     }
