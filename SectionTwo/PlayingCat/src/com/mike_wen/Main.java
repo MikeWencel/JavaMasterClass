3f22619a9304c0ce36b93bd86aaa31b3f274562b
MikeWen <wencel.michael@gmail.com>
2019-10-18 13:05:17 +0200

    Playing cat

---
+++ SectionTwo/PlayingCat/src/com/mike_wen/Main.java
@@ -0,0 +1,12 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static isCatPLaying(boolean summer,int temperature){
+        
+    }
+}
