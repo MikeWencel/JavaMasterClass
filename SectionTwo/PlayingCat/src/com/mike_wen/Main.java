14c264c4c2a8d5207e318a4a9a154ddf18df8703
MikeWen <wencel.michael@gmail.com>
2019-10-20 11:51:21 +0200

    Playing cat - done

--- SectionTwo/PlayingCat/src/com/mike_wen/Main.java
+++ SectionTwo/PlayingCat/src/com/mike_wen/Main.java
@@ -3,10 +3,18 @@ package com.mike_wen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	isCatPlaying(false,25);
     }
 
-    public static isCatPLaying(boolean summer,int temperature){
-        
+    public static boolean isCatPlaying(boolean summer,int temperature){
+    if ((summer) && temperature >= 25 && temperature <= 45){
+        System.out.println("Cat is playing");
+        return true;
+    }else if(!summer && temperature <= 35 && temperature >= 25){
+        System.out.println("Cat is playing couse is not summer");
+        return true;
+    }return false;
     }
-}
+
+
+    }
