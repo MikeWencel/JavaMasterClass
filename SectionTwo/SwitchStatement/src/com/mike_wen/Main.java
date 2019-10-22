6a67dafeb1d8fdbc027824e632179a9ccf0e009a
MikeWen <wencel.michael@gmail.com>
2019-10-22 07:48:31 +0200

    Switch statement with CHAR

--- SectionTwo/SwitchStatement/src/com/mike_wen/Main.java
+++ SectionTwo/SwitchStatement/src/com/mike_wen/Main.java
@@ -21,5 +21,31 @@ public class Main {
                 System.out.println("Value was not 1 or 2");
                 break;
         }
+
+
+        char value = 'D';
+
+
+        switch (value){
+            case 'A':
+                System.out.println("Value was A");
+                break;
+            case 'B':
+                System.out.println("Value was B");
+                break;
+            case 'C':
+                System.out.println("Value was C");
+                break;
+            case 'D':
+                System.out.println("Value was D");
+                break;
+            case 'E':
+                System.out.println("Value was E");
+                break;
+
+                default:
+                    System.out.println("Value was A,B,C,D or E");
+                    break;
+        }
     }
 }
