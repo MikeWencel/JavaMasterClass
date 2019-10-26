6fb2c98dd5ac8635ff34a82b223fbadc051ed6a7
MikeWen <wencel.michael@gmail.com>
2019-10-26 22:25:30 +0200

    Works perfect - finally

--- SectionTwo/SharedDigit/src/com/mike_wen/Main.java
+++ SectionTwo/SharedDigit/src/com/mike_wen/Main.java
@@ -3,10 +3,11 @@ package com.mike_wen;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(hasSharedDigit(22,72));
+        System.out.println(hasSharedDigit(22,23));
     }
 
     // Prawda jest taka, że nie potrzeba tu pętli :P, ale cóż.... jak się jest upartym osłem, to się próbuje :=) !!!
+    // Poniżej lepsze rozwiązanie :-)
 
     public static boolean hasSharedDigit(int first,int second){
         int numberOne = 0;
@@ -14,21 +15,17 @@ public class Main {
         int numberThree = 0;
         int numberFour = 0;
 
-        if (first < 10 || second > 99){
+        if (first < 10 || first > 99 || second > 99 || second < 10){
             return false;
         }else {
-         while (first > 0){
              numberOne = first % 10;
              first = first / 10;
              numberTwo = first % 10;
-             break;
-         }
 
-         while (second > 0){
              numberThree = second % 10;
              second = second / 10;
              numberFour = second % 10;
-             break;
+
 
          }
 
@@ -46,4 +43,4 @@ public class Main {
 
         }
     }
-}
+
