ce8a376bd0f92069f980010e193c2dc2a23aaabc
MikeWen <wencel.michael@gmail.com>
2019-10-28 20:26:18 +0100

    Works perfect

---
+++ SectionTwo/PerfectNumber/src/com/mike_wen/Main.java
@@ -0,0 +1,28 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(-1));
+    }
+
+    public static boolean isPerfectNumber (int number){
+        int factor;
+        int sum = 0;
+        if (number > 1){
+            for (int i = 1;i < number;i++){
+                if (number % i == 0){
+                    factor = i;
+                    sum = i + sum;
+
+
+                }
+
+            }if (sum == number){
+                return true;
+            }else {
+                return false;
+            }
+        }return false;
+    }
+}
