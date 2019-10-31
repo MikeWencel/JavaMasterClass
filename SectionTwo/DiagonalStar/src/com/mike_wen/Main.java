db5891f0c460c157b0a9c2881a921ac635217da1
MikeWen <wencel.michael@gmail.com>
2019-10-31 20:53:19 +0100

    Diagonal Star

---
+++ SectionTwo/DiagonalStar/src/com/mike_wen/Main.java
@@ -0,0 +1,18 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+	printSquareStar(5);
+    }
+    public static int printSquareStar (int number){
+        String star = "*";
+        if (number >= 5){
+            for(int i = 1;i < number;i++){
+                if (i == 1){
+                    System.out.println();
+                }
+            }
+        }return -1;
+    }
+}
