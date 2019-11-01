9153b8d238ae4d90190489e251c76dcc3520adbd
MikeWen <wencel.michael@gmail.com>
2019-11-01 12:17:20 +0100

    Diagonal Star - Done

--- SectionTwo/DiagonalStar/src/com/mike_wen/Main.java
+++ SectionTwo/DiagonalStar/src/com/mike_wen/Main.java
@@ -3,16 +3,23 @@ package com.mike_wen;
 public class Main {
 
     public static void main(String[] args) {
-	printSquareStar(5);
+	printSquareStar(15);
     }
-    public static int printSquareStar (int number){
+    public static void printSquareStar (int number){
         String star = "*";
+        String line = " ";
         if (number >= 5){
-            for(int i = 1;i < number;i++){
-                if (i == 1){
-                    System.out.println();
+            for(int i = 1;i <= number;i++){
+                for (int j = 1; j <= number;j++){
+                    if (i == 1 || j == 1 || i == number || j == number || i == j || j == number - i + 1){
+                        System.out.print(star);
+                    }else {
+                        System.out.print(line);
+                    }
                 }
+                System.out.println();
             }
-        }return -1;
+        }
+        System.out.println("Invalid Value");
     }
 }
