f7cae782bb2f1ccb069da22dbeeb72982160f0f5
MikeWen <wencel.michael@gmail.com>
2019-11-04 09:46:12 +0100

    Done

--- SectionTwo/ReadingUserInputChallange/src/com/mike_wen/Main.java
+++ SectionTwo/ReadingUserInputChallange/src/com/mike_wen/Main.java
@@ -7,14 +7,19 @@ public class Main {
     public static void main(String[] args) {
         int count = 0;
         int sum = 0;
+
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+
         boolean hasNextInt = scanner.hasNextInt();
 
         if (hasNextInt) {
+            int number = scanner.nextInt();
             while (count < 10) {
-                System.out.println("Enter your number: ");
-                int number = scanner.nextInt();
-                sum = number + number;
+                    System.out.println("Enter your number: ");
+                    number = scanner.nextInt();
+                    sum = number + sum;
+
                 count++;
             }
             System.out.println("Sum of your numbers is: "+ sum);
