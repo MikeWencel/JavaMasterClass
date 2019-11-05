041b98b084bd84ed64634d122856ec8f636c1e79
MikeWen <wencel.michael@gmail.com>
2019-11-05 09:11:18 +0100

    ver  0.8

---
+++ SectionTwo/MInAndMaxChallenge/src/com/mike_wen/Main.java
@@ -0,0 +1,17 @@
+package com.mike_wen;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    boolean hasNextInt = scanner.hasNextInt();
+
+    if (hasNextInt){
+        int number = scanner.nextInt()
+    }
+    }
+}
