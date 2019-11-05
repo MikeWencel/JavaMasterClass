6a545e4d062068bad4aa62eaf47abecb48fbc2bf
MikeWen <wencel.michael@gmail.com>
2019-11-05 09:44:34 +0100

    ver 0.9

--- SectionTwo/MInAndMaxChallenge/src/com/mike_wen/Main.java
+++ SectionTwo/MInAndMaxChallenge/src/com/mike_wen/Main.java
@@ -5,13 +5,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+    int count = 0;
+    int max = 0;
+    int min = 0;
+    int number = 0;
     Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Enter number:");
     boolean hasNextInt = scanner.hasNextInt();
 
     if (hasNextInt){
-        int number = scanner.nextInt()
+        while (count < 10) {
+            System.out.println("Enter number:");
+            number = scanner.nextInt();
+
+            System.out.println("Number is " + number);
+
+            if (number >= number) {
+                max = number;
+            }
+
+            if (number <= number) {
+                min = number;
+            }
+            count++;
+        }
     }
+
+
+
+        scanner.close();
     }
+
 }
