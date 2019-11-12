e0dcd65b6c8016d84cc3f9c6c6859e28cfa3b349
MikeWen <wencel.michael@gmail.com>
2019-11-12 18:58:31 +0100

    Input the number - Sum and average

---
+++ SectionTwo/InputCalc/src/com/mike_wen/Main.java
@@ -0,0 +1,18 @@
+package com.mike_wen;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+    public static void inputThenPrintSumAndAverange(){
+        Scanner scan = new Scanner(System.in);
+        boolean hasNextInt = true;
+    }
+}
