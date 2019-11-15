f7993377dd387fdf6e4e0798487b735510a07911
MikeWen <wencel.michael@gmail.com>
2019-11-15 09:24:49 +0100

    Working on it

--- SectionTwo/InputCalc/src/com/mike_wen/Main.java
+++ SectionTwo/InputCalc/src/com/mike_wen/Main.java
@@ -5,14 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
+        inputThenPrintSumAndAverange();
 
 
     }
 
-    public static void inputThenPrintSumAndAverange(){
+    public static void inputThenPrintSumAndAverange() {
+        int x;
+        int sum = 0;
         Scanner scan = new Scanner(System.in);
-        boolean hasNextInt = true;
+        System.out.println("Enter number: ");
+        boolean hasNextInt = scan.hasNextInt();
+
+        while (hasNextInt) {
+            System.out.println("Enter number: ");
+            hasNextInt = scan.hasNextInt();
+
+        }
+
+        scan.nextInt();
+        scan.close();
+
     }
 }
