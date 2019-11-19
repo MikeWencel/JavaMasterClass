8710c078435120abebc5758873e590bafe4b0508
MikeWen <wencel.michael@gmail.com>
2019-11-19 19:36:14 +0100

    Challenge done, three overloading methods - program counts how many liters, you need to do paint job

--- SectionTwo/PaintJob/src/com/mike_wen/Main.java
+++ SectionTwo/PaintJob/src/com/mike_wen/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
 	System.out.println(getBucketCount(3.4,2.1,1.5));
 	System.out.println(getBucketCount(7.25,4.3,2.35));
+	System.out.println(getBucketCount(3.26,0.75));
     }
     public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
         double area;
@@ -34,5 +35,15 @@ public class Main {
            return bucketCover;
         }
 
+    }
+
+    public static int getBucketCount (double area, double areaPerBucket){
+        if ((area <= 0 || areaPerBucket <= 0 )){
+            return -1;
+        }else {
+            double bucketArea = area/areaPerBucket;
+            int buckets = (int) Math.ceil(bucketArea);
+            return buckets;
+        }
     }
 }
