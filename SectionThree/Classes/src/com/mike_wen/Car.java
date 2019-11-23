cfec2040503fda5b9a8fb3110570ef1bbd538de0
MikeWen <wencel.michael@gmail.com>
2019-11-23 18:02:13 +0100

    Java - Classes part 1/2

---
+++ SectionThree/Classes/src/com/mike_wen/Car.java
@@ -0,0 +1,13 @@
+package com.mike_wen;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public
+}
+
+
---
+++ SectionThree/Classes/src/com/mike_wen/Main.java
@@ -0,0 +1,14 @@
+package com.mike_wen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porshe =  new Car();
+        Car mercedes = new Car();
+
+        //accesing variables - different between private and public
+
+        porshe.model = "Carrera";
+
+    }
+}
