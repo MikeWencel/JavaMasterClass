146816021b448bf89b17f4ab1242a67e32e7840a
MikeWen <wencel.michael@gmail.com>
2019-11-25 22:43:14 +0100

    ver 0.6

--- SectionThree/SumCalculator/src/com/mike_wen/SimpleCalculator.java
+++ SectionThree/SumCalculator/src/com/mike_wen/SimpleCalculator.java
@@ -1,4 +1,18 @@
 package com.mike_wen;
 
 public class SimpleCalculator {
-}
+    public int firstNumber;
+    public int secondNumber;
+
+    public int getFirstNumber(int firstNumber){
+        return this.firstNumber;
+    }
+
+    public int getSecondNumber(int secondNumber){
+        return this.secondNumber;
+    }
+
+    public int setFirstNumber(double number)
+
+    }
+
