f9fd20ba47aced6c488f0fca0d1010f87a361342
MikeWencel <wencel.michael@gmail.com>
2020-03-22 16:19:59 +0100

    Correction

--- SectionTwo/ReadingUserInput/src/com/mike_wen/Main.java
+++ SectionTwo/ReadingUserInput/src/com/mike_wen/Main.java
@@ -6,31 +6,36 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner scanner= new Scanner(System.in);
 
+        System.out.println("Enter your year of birth");
 
-        System.out.println("Enter you year of birth");
+        boolean hasNextInt = scanner.hasNextInt();
 
-        boolean hasNExtInt = scanner.hasNextInt();
-
-
-        if (hasNExtInt) {
+        if (hasNextInt){
             int yearOfBirth = scanner.nextInt();
-            scanner.nextLine(); // handle next line charater (enter key)
+            scanner.nextLine(); // After scanning number, always we must write nexLine, to handle of working scanner
 
-            System.out.println("Enter you name: ");
+
+            System.out.println("Enter your name: ");
             String name = scanner.nextLine();
-            int age = 2019 - yearOfBirth;
 
-            if (age < 100 && age > 0) {
-                System.out.println("Your name is " + name + " and you're " + age + " years old");
-            } else {
-                System.out.println(name + " that's impossible, write real date of birth");
+
+            int year = 2020;
+            int ageOfTheUser = year - yearOfBirth;
+            if (ageOfTheUser >= 0 && ageOfTheUser <= 100){
+                System.out.println("Your name is " + name + " and you are " + ageOfTheUser + " years old!");
+            }else {
+                System.out.println("Ivalid year of birth");
             }
-
-            scanner.close();
         }else {
-            System.out.println("Try again !!");
+            System.out.println("This isn't correct datatype");
         }
 
+
+
+
+
+
+        scanner.close();
 
     }
 }
