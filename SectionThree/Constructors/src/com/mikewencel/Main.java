d4c6d7a1f1241b7ac86329e95e552246a568b532
MikeWencel <wencel.michael@gmail.com>
2020-04-06 20:34:38 +0200

    constructors

--- SectionThree/Constructors/src/com/mikewencel/Main.java
+++ SectionThree/Constructors/src/com/mikewencel/Main.java
@@ -4,23 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-    bankAccount firstAccount = new bankAccount();
+    bankAccount baobabAccount = new bankAccount("232323",5000,"Baobab the cat","baobabthemail@gmail.com","34213213");
+    bankAccount KateAccount = new bankAccount("14 5600 3323 0000 0000", 20000,"Kate the cat","Littlecat@gmail.com","324214214");
+    bankAccount Timsaccount = new bankAccount("Tim","tima@fasfda2.com","56600032");
+    baobabAccount.deposit(230);
+    baobabAccount.withdrawal(1340);
+    KateAccount.myBalance();
+    KateAccount.deposit(4756);
+    System.out.println(Timsaccount.getNumber() + " name " + Timsaccount.getCustomerName());
 
-    firstAccount.setCustomerName("Baobab");
-        System.out.println(firstAccount.getCustomerName());
-    firstAccount.setNumber("14 0000 2322 5455 2768");
-    firstAccount.setBalance(5000);
-    firstAccount.deposit(5000);
-        System.out.println(firstAccount.getBalance());
 
-
-        System.out.println("Customer name: "
-                + firstAccount.getCustomerName() +
-                " account number is " +
-                firstAccount.getNumber());
-
-        firstAccount.withdrawal(150);
-        System.out.println(firstAccount.getBalance());
 
     }
 
--- SectionThree/Constructors/src/com/mikewencel/bankAccount.java
+++ SectionThree/Constructors/src/com/mikewencel/bankAccount.java
@@ -8,21 +8,43 @@ public class bankAccount {
     private String customerEmailAddress;
     private String customerPhoneNumber;
 
+    public bankAccount() {
+        System.out.println("empty constructor called");
+    }
+
+    public bankAccount(String number, double balance, String customerName, String customerEmailAddress, String customerPhoneNumber){
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerEmailAddress = customerEmailAddress;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
 
     public void deposit(double depositAmount){
-        this.balance =+ depositAmount;
+        this.balance = balance + depositAmount;
+        System.out.println("You deposit " + depositAmount + " cash. Your actuall bank balance is " + this.balance);
+
+    }
+
+    public bankAccount(String customerName, String customerEmailAddress, String customerPhoneNumber) {
+        this("99999",0.0,customerName,customerEmailAddress,customerPhoneNumber);
 
     }
 
     public void withdrawal(double withdrawalAmount){
-        if (withdrawalAmount > this.balance){
-            System.out.println("Insufficient money on your bank account");
+        if (this.balance - withdrawalAmount < 0){
+            System.out.println("Insufficient money on your bank account, only balance available: " + this.balance);
         }else {
             this.balance = balance - withdrawalAmount;
-            System.out.println(this.customerName + " withdrawal: " + withdrawalAmount);
+            System.out.println(this.customerName + " withdrawal: " + withdrawalAmount + " actual balance is: " + this.balance);
         }
 
     }
+
+    public void myBalance(){
+        System.out.println("Your actual bank balance is: " + balance);
+    }
+
 
     public String getNumber(){
         return number;
@@ -38,7 +60,7 @@ public class bankAccount {
 
     public void setBalance(double balance) {
         this.balance = balance;
-        System.out.println("actual bank balance is: " + balance);
+
     }
 
     public String getCustomerName(){
