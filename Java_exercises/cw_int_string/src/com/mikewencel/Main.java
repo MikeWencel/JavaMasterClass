abc06d8db68868e7512bfba2c65909305777bb1c
MikeWencel <wencel.michael@gmail.com>
2020-04-27 22:43:47 +0200

    Done - code wars

---
+++ Java_exercises/cw_int_string/src/com/mikewencel/Main.java
@@ -0,0 +1,13 @@
+package com.mikewencel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(numberToString(10));
+    }
+
+    public static String numberToString(int num) {
+        String i = String.valueOf(num);
+        return i;
+    }
+}
