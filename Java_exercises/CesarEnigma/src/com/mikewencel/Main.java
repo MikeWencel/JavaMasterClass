93d73091c36a58187f2bcb61a611c9a6b3a0364f
unknown <MikeWencel>
2020-10-06 21:26:49 +0200

    You're doing good!

--- Java_exercises/CesarEnigma/src/com/mikewencel/Main.java
+++ Java_exercises/CesarEnigma/src/com/mikewencel/Main.java
@@ -5,12 +5,27 @@ public class Main {
     public static void main(String[] args) {
 
         char c;
-        String[] letter;
-        int i;
 
+
+        int i = 0;
         for (c = 'A'; c <= 'Z'; ++c) {
-
+            System.out.println(c + " ");
+            i++;
         }
+
+        System.out.println(i);
+
+        char[]alphabet = new char [i];
+        int j = 0;
+        for (c = 'A'; c <= 'Z'; ++c) {
+            alphabet[j] = c;
+            j++;
+        }
+
+        System.out.println(alphabet[1]);
+
+
+
 
 
 
