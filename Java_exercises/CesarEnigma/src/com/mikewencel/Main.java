a708ec2a46500662610a9706aef206b194fcfddc
unknown <MikeWencel>
2020-10-06 19:44:43 +0200

    Alphabet v 1.0

--- Java_exercises/CesarEnigma/src/com/mikewencel/Main.java
+++ Java_exercises/CesarEnigma/src/com/mikewencel/Main.java
@@ -5,9 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         char c;
+        String[] letter;
+        int i;
 
         for (c = 'A'; c <= 'Z'; ++c) {
-            System.out.println(c + " ");
+
         }
+
+
+
     }
 }
