723f2d5261f9abe3923892f32dc7a5946f26eea7
unknown <MikeWencel>
2020-10-12 21:05:56 +0200

    You're doing great!

--- SectionFive_Arrays/Array_Challenge/src/com/mikewencel/Main.java
+++ SectionFive_Arrays/Array_Challenge/src/com/mikewencel/Main.java
@@ -1,14 +1,20 @@
 package com.mikewencel;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-//        System.out.println(getIntegers());
-        writeArray(getIntegers());
+
+        int finalArray[] = sortedArray(getIntegers());
+        descentArray(finalArray);
+        writeArray(finalArray);
+
+
     }
+
 
     public static int[] getIntegers(){
         System.out.println("How long is your array");
@@ -26,5 +32,28 @@ public class Main {
             System.out.println(element);
         }
     }
+
+    public static int[] sortedArray(int array[]){
+
+        Arrays.sort(array);
+
+        return array;
+    }
+
+    public static int[] descentArray(int array[]){
+        int newbie [] = new int[array.length];
+        int c = 0;
+        for(int i = array.length -1;i >= 0;i--){
+            newbie[c] = array[i];
+            c++;
+        }
+        for(int i = 0; i < newbie.length;i++){
+            array[i] = newbie[i];
+        }
+
+        return array;
+    }
+
+
 
 }
