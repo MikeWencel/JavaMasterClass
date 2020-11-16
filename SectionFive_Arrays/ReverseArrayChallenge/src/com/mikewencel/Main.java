38c2e34f5c447cafb63873205d8a35d782e1cfe4
unknown <MikeWencel>
2020-11-16 19:32:16 +0100

    You're doing great!

--- SectionFive_Arrays/ReverseArrayChallenge/src/com/mikewencel/Main.java
+++ SectionFive_Arrays/ReverseArrayChallenge/src/com/mikewencel/Main.java
@@ -1,11 +1,16 @@
 package com.mikewencel;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
     int [] tablica = {1,2,3,4,5};
+
     printTab(tablica);
-    reverse(tablica);
+    reverseSecond(tablica);
+
+        System.out.println("Array reversed in second version = " + Arrays.toString(tablica));
     }
 
     static void reverse(int array[]){
@@ -28,4 +33,15 @@ public class Main {
             System.out.println(el);
         }
     }
+
+    static void reverseSecond(int[] array) {
+        int maxIndex = array.length -1;
+        int halfLength = array.length / 2;
+        for (int i = 0; i < halfLength;i++){
+            int temp = array[i];
+            array[i] = array[maxIndex -i];
+            array[maxIndex - i] = temp;
+        }
+    }
+
 }
