405de1c4766ef3953a04076621e2b37cbfa4e119
Ventillo <MikeWencel>
2020-11-24 22:35:41 +0100

    You're doing great!

--- Java_exercises/Stos/src/com/mikewencel/Main.java
+++ Java_exercises/Stos/src/com/mikewencel/Main.java
@@ -1,5 +1,6 @@
 package com.mikewencel;
 
+import java.lang.*;
 import java.util.Scanner;
 
 public class Main {
@@ -10,36 +11,38 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-	int i = 0;
-	char s = ' ';
-	int [] tab = new int [10];
+        int i = 0;
+        char s = ' ';
+        int [] tab = new int [10];
 
 
 
-	while (scanner.hasNext()){
+        while (scanner.hasNext()){
+            s = scanner.next().charAt(0);
+            int ascii = s;
+            if(ascii == 13){
+                scanner.close();
+            }else {
+                if (s == '+') {
+                    if (i < 10) {
+                        tab[i] = scanner.nextInt();
+                        System.out.println(":)");
+                        i++;
+                    } else {
+                        System.out.println(":(");
+                    }
+                } else if (s == '-') {
+                    if (i <= 10 && i > 0) {
+                        System.out.println(tab[i - 1]);
+                        tab[i - 1] = 0;
+                        i--;
 
-	    s = scanner.next().charAt(0);
-	    if(s == '+'){
-            if(i < 10){
-                tab[i] = scanner.nextInt();
-                System.out.println(":)");
-                i++;
-            }
-        else{
-                System.out.println(":(");
-            }
-        }else if(s == '-'){
-            if(i < 10 && i > 0){
-                i--;
-            }
-            else{
-                System.out.println(":(");
+                    } else {
+                        System.out.println(":(");
+                    }
+                }
+
             }
         }
-
-    }
-
-    scanner.close();
-
     }
 }
