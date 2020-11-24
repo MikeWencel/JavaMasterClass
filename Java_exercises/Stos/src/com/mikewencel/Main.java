179679aad821d039011f88ce10888f8ff38ad1eb
Ventillo <MikeWencel>
2020-11-24 18:45:15 +0100

    You're doing great!

--- Java_exercises/Stos/src/com/mikewencel/Main.java
+++ Java_exercises/Stos/src/com/mikewencel/Main.java
@@ -1,8 +1,45 @@
 package com.mikewencel;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+
+
     public static void main(String[] args) {
-	int tab [] = new int [10];
+
+        Scanner scanner = new Scanner(System.in);
+	int i = 0;
+	char s = ' ';
+	int [] tab = new int [10];
+
+
+
+	while (scanner.hasNext()){
+
+	    s = scanner.next().charAt(0);
+	    if(s == '+'){
+            if(i < 10){
+                tab[i] = scanner.nextInt();
+                System.out.println(":)");
+                i++;
+            }
+        else{
+                System.out.println(":(");
+            }
+        }else if(s == '-'){
+            if(i < 10 && i > 0){
+                i--;
+            }
+            else{
+                System.out.println(":(");
+            }
+        }
+
+    }
+
+    scanner.close();
+
     }
 }
