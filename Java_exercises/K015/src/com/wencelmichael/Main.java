b9cd7aa375a0abfefb4df79be6c66069ee0279ac
MikeWencel <wencel.michael@gmail.com>
2020-11-26 21:04:02 +0100

    You're doing great!

---
+++ Java_exercises/K015/src/com/wencelmichael/Main.java
@@ -0,0 +1,33 @@
+package com.wencelmichael;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Tworze tablicę
+        String arr [] = new String[3];
+        //Tworzę pętle w której dodaje 3 elementy do stringa
+
+        //dodaje elementy do stringa
+
+        for(int i =0;i < arr.length-1;i++){
+            String str = scanner.nextLine();
+            arr[i] = str;
+        }
+
+
+        for (element: arr) {
+
+        }
+
+
+
+
+//        System.out.println(str);
+//        int i = Integer.parseInt(str);
+//        System.out.println(i);
+    }
+}
