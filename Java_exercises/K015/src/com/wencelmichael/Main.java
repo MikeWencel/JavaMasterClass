0836a721b57d3ebd349de9288e4f03962b28eb8f
MikeWencel <wencel.michael@gmail.com>
2020-11-28 14:30:39 +0100

    You're doing great!

--- Java_exercises/K015/src/com/wencelmichael/Main.java
+++ Java_exercises/K015/src/com/wencelmichael/Main.java
@@ -1,33 +1,69 @@
 package com.wencelmichael;
 
 import java.util.Scanner;
+import java.lang.String;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //Tworze tablicę
-        String arr [] = new String[3];
-        //Tworzę pętle w której dodaje 3 elementy do stringa
-
-        //dodaje elementy do stringa
-
-        for(int i =0;i < arr.length-1;i++){
+        while (scanner.hasNext()) {
             String str = scanner.nextLine();
-            arr[i] = str;
+            String[] tab = str.split(" ");
+
+
+            if (tab[0].length() == tab[2].length()) {
+                char[] first = new char[tab[0].length()];
+                for (int i = 0; i < tab[0].length(); i++) {
+                    first[i] = tab[0].charAt(i);
+                }
+                char[] second = new char[tab[2].length()];
+                for (int i = 0; i < tab[2].length(); i++) {
+                    second[i] = tab[2].charAt(i);
+                }
+
+                switch (tab[1]) {
+                    case "==": {
+                        for (int i = 0; i < first.length; i++) {
+                            if (first[i] != second[i]) {
+                                System.out.println(0);
+                                break;
+                            } else {
+                                //String flaga
+                            }
+                        }
+                    }
+                    break;
+                    case ">=": {
+                        for (int i = 0; i < first.length; i++) {
+                            if (first[i] <= second[i]) {
+                                System.out.println(0);
+                                break;
+                            } else {
+                                System.out.println(1);
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                    case "<=": {
+                        for (int i = 0; i < first.length; i++) {
+                            if (first[i] >= second[i]) {
+                                System.out.println(0);
+                                break;
+                            } else {
+                                System.out.println(1);
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+            }
+        scanner.close();
         }
 
-
-        for (element: arr) {
-
-        }
-
-
-
-
-//        System.out.println(str);
-//        int i = Integer.parseInt(str);
-//        System.out.println(i);
     }
-}
+
