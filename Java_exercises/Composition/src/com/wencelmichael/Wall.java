48fdc5eecee4030b1cbc6242f5d3f7c78679774e
MikeWencel <wencel.michael@gmail.com>
2020-12-14 18:54:10 +0100

    Done, you're doing great!

---
+++ Java_exercises/Composition/src/com/wencelmichael/Wall.java
@@ -0,0 +1,13 @@
+package com.wencelmichael;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
