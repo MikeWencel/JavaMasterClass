e435d4d05512f66267bbcfca586fa75af33e7b57
MikeWencel <wencel.michael@gmail.com>
2021-01-14 17:53:21 +0100

    Done

---
+++ SelfTaught/YourOwnAge/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import sun.util.resources.LocaleData;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocaleData currentData = new LocaleData();
+
+    }
+//
+//    public static int Age(int dateOfBirth){
+//        int daten =
+//    }
+}
